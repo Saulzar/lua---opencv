f040f717dacbd76f6d7fd9422d10900c061cf533
Oliver Batchelor <saulzar@gmail.com>
2014-06-06 02:21:32 +1200

    Butcher and begin again with simpler generic image conversions

--- opencv.c
+++ opencv.c
@@ -444,6 +444,17 @@ static int libopencv_videoGetMSEC(lua_State *L) {
   return 1;
 }
 
+
+static int qualityFlags(int index) {
+  switch(index) {
+   case 0: return CV_INTER_NN;
+   case 2: return CV_INTER_CUBIC;
+   case 3: return CV_INTER_AREA;
+   case 4: return CV_INTER_LANCZOS4;   
+   default: return CV_INTER_LINEAR;
+  }
+}
+
 //============================================================
 // Register functions in LUA
 //
@@ -470,13 +481,17 @@ static const luaL_reg libopencv_init [] =
 #define libopencv_(NAME) TH_CONCAT_3(libopencv_, Real, NAME)
 
 #include "generic/opencv.c"
-#include "THGenerateFloatTypes.h"
+#include "THGenerateAllTypes.h"
+
 
 DLL_EXPORT int luaopen_libopencv(lua_State *L)
 {
 
   luaL_register(L, "opencv", libopencv_init);
 
+  libopencv_ByteMain_init(L);
+  libopencv_CharMain_init(L);
+  libopencv_LongMain_init(L);
   libopencv_FloatMain_init(L);
   libopencv_DoubleMain_init(L);
 
